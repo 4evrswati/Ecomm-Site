ec78d702475b10a5382a5ab770ae1155738228e1
Swati <4evr.swati@gmail.com>
2023-08-14 03:04:55 +0530

    Final Changes

--- backend/index.js
+++ backend/index.js
@@ -6,22 +6,26 @@ const authRoutes = require('./routes/authRoute')
 const categoryRoutes = require('./routes/categoryRoutes')
 const productRoutes = require('./routes/productRoutes')
 const cors = require('cors')
+const path = require('path')
 
-const PORT = process.env.PORT || 4000;
-
+//middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './frontend/build')))
+
 dbConnect();
 
+//routes
 app.use('/api/user', authRoutes);
 app.use('/api/category', categoryRoutes)
 app.use('/api/product', productRoutes)
 
-app.use('/',(req, res) => {
-    res.send('Hi..')
+//rest API
+app.use('*',function(req, res){
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'))
 })
 
-
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log('Server is listening at port ' + PORT);
